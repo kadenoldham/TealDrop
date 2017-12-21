7ccba8401ee6945f0e4306cdca9dcf3e3f7edad1
kadenoldham <shmedly24@gmail.com>
2017-12-21 14:45:02 -0700

    updated MARKS

--- DropIn/ViewControllers/ImageCollectionViewController.swift
+++ DropIn/ViewControllers/ImageCollectionViewController.swift
@@ -327,51 +327,37 @@ class ImageCollectionViewController: ShiftableViewController, UICollectionViewDe
 }
 extension UIImage {
     
+    //MARK: - fixOrientation
     func fixOrientation() -> UIImage {
-        
-        // No-op if the orientation is already correct
         if ( self.imageOrientation == UIImageOrientation.up ) {
             return self;
         }
-        
-        // We need to calculate the proper transformation to make the image upright.
-        // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
         var transform: CGAffineTransform = CGAffineTransform.identity
-        
         if ( self.imageOrientation == UIImageOrientation.down || self.imageOrientation == UIImageOrientation.downMirrored ) {
             transform = transform.translatedBy(x: self.size.width, y: self.size.height)
             transform = transform.rotated(by: CGFloat(Double.pi))
         }
-        
         if ( self.imageOrientation == UIImageOrientation.left || self.imageOrientation == UIImageOrientation.leftMirrored ) {
             transform = transform.translatedBy(x: self.size.width, y: 0)
             transform = transform.rotated(by: CGFloat(Double.pi / 2.0))
         }
-        
         if ( self.imageOrientation == UIImageOrientation.right || self.imageOrientation == UIImageOrientation.rightMirrored ) {
             transform = transform.translatedBy(x: 0, y: self.size.height);
             transform = transform.rotated(by: CGFloat(-Double.pi / 2.0));
         }
-        
         if ( self.imageOrientation == UIImageOrientation.upMirrored || self.imageOrientation == UIImageOrientation.downMirrored ) {
             transform = transform.translatedBy(x: self.size.width, y: 0)
             transform = transform.scaledBy(x: -1, y: 1)
         }
-        
         if ( self.imageOrientation == UIImageOrientation.leftMirrored || self.imageOrientation == UIImageOrientation.rightMirrored ) {
             transform = transform.translatedBy(x: self.size.height, y: 0);
             transform = transform.scaledBy(x: -1, y: 1);
         }
-        
-        // Now we draw the underlying CGImage into a new context, applying the transform
-        // calculated above.
         let ctx: CGContext = CGContext(data: nil, width: Int(self.size.width), height: Int(self.size.height),
                                        bitsPerComponent: self.cgImage!.bitsPerComponent, bytesPerRow: 0,
                                        space: self.cgImage!.colorSpace!,
                                        bitmapInfo: self.cgImage!.bitmapInfo.rawValue)!;
-        
         ctx.concatenate(transform)
-        
         if ( self.imageOrientation == UIImageOrientation.left ||
             self.imageOrientation == UIImageOrientation.leftMirrored ||
             self.imageOrientation == UIImageOrientation.right ||
@@ -380,8 +366,6 @@ extension UIImage {
         } else {
             ctx.draw(self.cgImage!, in: CGRect(x: 0,y: 0,width: self.size.width,height: self.size.height))
         }
-        
-        // And now we just create a new UIImage from the drawing context and return it
         return UIImage(cgImage: ctx.makeImage()!)
     }
 }
